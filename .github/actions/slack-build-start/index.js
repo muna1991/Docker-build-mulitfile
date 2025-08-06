d713efbd6783840eb77e9761d7294c8d25c6f28e
m-mahapatra_sfemu <m.mahapatra@salesforce.com>
2025-08-06 10:20:29 +0530

    updated core logic

--- .github/actions/slack-build-start/index.js
+++ .github/actions/slack-build-start/index.js
@@ -7,16 +7,18 @@ const { WebClient } = require('@slack/web-api');
     const channel = core.getInput('channel_id');
     const tag = core.getInput('image_tag');
     const repo = core.getInput('repository');
+    const environment = core.getInput('environment');
 
     await slack.chat.postMessage({
         channel,
-        text: `🚀 Docker build started for ${repo}:${tag}`,
+        text: `🚀 Docker build started for ${repo}:${tag} in ${environment}`,
         blocks: [
             { type: 'header', text: { type: 'plain_text', text: '🚀 Docker Build Started', emoji: true } },
             {
                 type: 'section', fields: [
                     { type: 'mrkdwn', text: `*📦 Repository:*\n\`${repo}\`` },
-                    { type: 'mrkdwn', text: `*🏷️ Tag:*\n\`${tag}\`` }
+                    { type: 'mrkdwn', text: `*🏷️ Tag:*\n\`${tag}\`` },
+                    { type: 'mrkdwn', text: `*🌐 Environment:*\n\`${environment}\`` }  // 👈 Add this
                 ]
             },
             { type: 'context', elements: [{ type: 'mrkdwn', text: '🔧 Build in progress...' }] }
